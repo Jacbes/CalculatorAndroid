0f10d266d68b55d1f95e39b63963e24bc6e45b9a
Jacbes <skishon100000@gmail.com>
2022-10-18 20:34:26 +0700

    Change type of number to double

--- app/src/main/java/dev/jacbes/calculatorandroid/MainActivity.java
+++ app/src/main/java/dev/jacbes/calculatorandroid/MainActivity.java
@@ -7,86 +7,128 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.function.IntBinaryOperator;
+import java.text.DecimalFormat;
+import java.util.function.DoubleBinaryOperator;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView numberTextView;
+    private static final DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+    TextView resultTextView;
     String firstNumber = "";
     String secondNumber = "";
-    Integer result = 0;
-    IntBinaryOperator operation = null;
+    String resultNumber = "";
+    DoubleBinaryOperator operation = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        numberTextView = findViewById(R.id.numbers_textview);
+        resultTextView = findViewById(R.id.numbers_textview);
 
         Button oneButton = findViewById(R.id.one_button);
-        oneButton.setOnClickListener(view -> addNumberToTextView("1"));
+        oneButton.setOnClickListener(view -> addToSecondNumber("1"));
         Button twoButton = findViewById(R.id.two_button);
-        twoButton.setOnClickListener(view -> addNumberToTextView("2"));
+        twoButton.setOnClickListener(view -> addToSecondNumber("2"));
         Button threeButton = findViewById(R.id.three_button);
-        threeButton.setOnClickListener(view -> addNumberToTextView("3"));
+        threeButton.setOnClickListener(view -> addToSecondNumber("3"));
         Button fourButton = findViewById(R.id.four_button);
-        fourButton.setOnClickListener(view -> addNumberToTextView("4"));
+        fourButton.setOnClickListener(view -> addToSecondNumber("4"));
         Button fiveButton = findViewById(R.id.five_button);
-        fiveButton.setOnClickListener(view -> addNumberToTextView("5"));
+        fiveButton.setOnClickListener(view -> addToSecondNumber("5"));
         Button sixButton = findViewById(R.id.six_button);
-        sixButton.setOnClickListener(view -> addNumberToTextView("6"));
+        sixButton.setOnClickListener(view -> addToSecondNumber("6"));
         Button sevenButton = findViewById(R.id.seven_button);
-        sevenButton.setOnClickListener(view -> addNumberToTextView("7"));
+        sevenButton.setOnClickListener(view -> addToSecondNumber("7"));
         Button eightButton = findViewById(R.id.eight_button);
-        eightButton.setOnClickListener(view -> addNumberToTextView("8"));
+        eightButton.setOnClickListener(view -> addToSecondNumber("8"));
         Button nineButton = findViewById(R.id.nine_button);
-        nineButton.setOnClickListener(view -> addNumberToTextView("9"));
+        nineButton.setOnClickListener(view -> addToSecondNumber("9"));
         Button zeroButton = findViewById(R.id.zero_button);
-        zeroButton.setOnClickListener(view -> addNumberToTextView("0"));
+        zeroButton.setOnClickListener(view -> addToSecondNumber("0"));
 
         Button sumButton = findViewById(R.id.sum_button);
-        sumButton.setOnClickListener(view -> writeNumber((x, y) -> x + y));
+        sumButton.setOnClickListener(view -> setOperation((x, y) -> x + y));
         Button subButton = findViewById(R.id.sub_button);
-        subButton.setOnClickListener(view -> writeNumber((x, y) -> x - y));
+        subButton.setOnClickListener(view -> setOperation((x, y) -> x - y));
         Button divideButton = findViewById(R.id.divide_button);
-        divideButton.setOnClickListener(view -> writeNumber((x, y) -> x / y));
+        divideButton.setOnClickListener(view -> setOperation((x, y) -> x / y));
         Button multiplyButton = findViewById(R.id.multiply_button);
-        multiplyButton.setOnClickListener(view -> writeNumber((x, y) -> x * y));
+        multiplyButton.setOnClickListener(view -> setOperation((x, y) -> x * y));
         Button resultButton = findViewById(R.id.result_button);
-        resultButton.setOnClickListener(view -> calculateOperation());
+        resultButton.setOnClickListener(view -> launchOperation());
+
+        Button dotButton = findViewById(R.id.dot_button);
+        dotButton.setOnClickListener(view -> setDotInSecondNumber());
+
+        Button eraseNumberButton = findViewById(R.id.erase_number_button);
+        eraseNumberButton.setOnClickListener(view -> eraseSecondNumber());
     }
 
-    private void addNumberToTextView(String number) {
+    private void eraseSecondNumber() {
+        if (!secondNumber.isEmpty()) {
+            setSecondNumber(secondNumber.substring(0, secondNumber.length() - 1));
+            setResultTextView(secondNumber);
+        }
+    }
+
+    private void setSecondNumber(String number) {
+        secondNumber = number;
+    }
+
+    private void setResultTextView(String number) {
+        resultTextView.setText(number);
+    }
+
+    private void setDotInSecondNumber() {
+        if (!checkDotInSecondNumber()) {
+            addToSecondNumber(".");
+        }
+    }
+
+    private boolean checkDotInSecondNumber() {
+        return secondNumber.contains(".");
+    }
+
+    private void addToSecondNumber(String number) {
         secondNumber = secondNumber + number;
-
-        numberTextView.setText(secondNumber);
+        setResultTextView(secondNumber);
     }
 
-    private void writeNumber(IntBinaryOperator operation) {
+    private void setOperation(DoubleBinaryOperator operation) {
         this.operation = operation;
 
-        firstNumber = secondNumber;
-        secondNumber = "";
-        numberTextView.setText("");
+        setFirstNumber(secondNumber);
+        setResultTextView("");
     }
 
-    private void calculateOperation() {
+    private void setFirstNumber(String number) {
+        firstNumber = number;
+        secondNumber = "";
+    }
+
+    private void launchOperation() {
         if (operation != null) {
             try {
-                result = operation.applyAsInt(Integer.parseInt(firstNumber), Integer.parseInt(secondNumber));
+                Double resultOfOperation = operation
+                        .applyAsDouble(
+                                Double.parseDouble(firstNumber),
+                                Double.parseDouble(secondNumber));
+                this.resultNumber = decimalFormat.format(resultOfOperation);
 
-                numberTextView.setText(String.valueOf(result));
-            } catch (ArithmeticException e) {
-                if (secondNumber.equals("0")) {
-                    numberTextView.setText("∞");
-                }
+                setResultTextView(this.resultNumber);
             } catch (Exception e) {
-                Toast.makeText(getApplicationContext(), "Error to calculate", Toast.LENGTH_LONG)
-                        .show();
+                Toast.makeText(getApplicationContext(),
+                        "Error to calculate",
+                        Toast.LENGTH_LONG).show();
             }
         }
 
+        clearNumbersAndOperation();
+    }
+
+    private void clearNumbersAndOperation() {
         firstNumber = "";
         secondNumber = "";
         operation = null;
